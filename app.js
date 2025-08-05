cdaea6d1465c6e86a64c34592379de4c20c1b7db
Enrique Abrham Céspedes Domínguez <49700033+dropdream@users.noreply.github.com>
2025-08-05 12:23:12 -0400

    implementar lógica completa para sorteo de amigo secreto
    Se desarrolló la lógica principal del proyecto "Amigo Secreto" en JavaScript:
    - Agregar nombres de amigos a un array y mostrarlos en una lista dinámica.
    - Sortear nombres al azar sin repeticiones utilizando un array de control.
    - Validaciones para evitar entradas vacías y evitar sorteos sin datos.
    - Ocultamiento de la lista una vez iniciado el sorteo.
    - Inclusión de botón para repetir el sorteo hasta agotar los nombres.
    
    Esta implementación refuerza el manejo del DOM, arrays y control de flujo en JS.

--- app.js
+++ app.js
@@ -1,1 +1,79 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//arreglo de amigos
+let amigos = [];
+
+//arreglo para que no se repitan los amigos
+let indiceArray = [];
+
+//Funcion que añade amigos
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (!nombreAmigo) {
+        alert('Debe ingresar el nombre de un amigo');
+        return;
+    } else {
+        //Se añade nombre al array de amigos
+        amigos.push(nombreAmigo);
+
+        //Muestra listado abajo
+        mostrarAmigos();
+
+
+        console.log(amigos);
+    }
+}
+
+//Funcion para mostrar amigos en listado
+function mostrarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+
+    //Limpiamos lista
+    listaAmigos.innerHTML = '';
+
+    //Recorremos y añadimos el listado
+    for (let i = 0; i < amigos.length; i++) {
+        let listado = document.createElement('li');
+        listado.textContent = amigos[i];
+        listaAmigos.appendChild(listado);
+        document.getElementById('amigo').value = "";
+    }
+}
+
+//Funcion para sortear amigos
+function sortearAmigo() {
+    cantidadAmigos = amigos.length;
+
+    if (cantidadAmigos > 0) {
+        if (indiceArray.length === amigos.length) {
+            alert('Se han sorteado todos los amigos');
+            return;
+        }
+
+        //Indice para sortear amigo
+        let indice = Math.floor(Math.random() * amigos.length);
+
+        if (indiceArray.includes(indice)) {
+            //Vuelve a ejecutar funcion para intentar con otro ya que este amigo ya salio.
+            sortearAmigo();
+        } else {
+            //Guardamos en un array el amigo sorteado para que no salga nuevamente
+            indiceArray.push(indice);
+
+            let nombre = amigos[indice];
+
+            //Ocultamos listado y mostramos el resultado del sorteo
+            document.getElementById('listaAmigos').setAttribute('hidden', true);
+            document.getElementById('realizarSorteo').setAttribute('hidden', true);
+            
+            //Boton que repite sorteo hasta que se agotan los nombres
+            document.getElementById('resultado').innerHTML = ` <p>${nombre}</p>
+                                                            <br>
+                                                            <button class="button-draw" onclick="sortearAmigo()" id="repetirSorteo">Elegir nuevamente</button>`;
+        }
+
+    } else {
+        //Alerta si el arreglo esta vacio
+        alert('Debe ingresar amigos para realizar el sorteo');
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,39 @@
+#Amigo Secreto - Challenge Alura Latam - ONE
+##Funcionalidades
+- Agregar nombres de amigos a una lista
+- Muestra los nombres ingresados de manera dinamica.
+- Sorte nombres al azar
+- Evita que se repitan los nombres sorteados
+- Oculta la lista de nombres y muestra el resultado del sorteo
+- Boton para seguir sorteando hasta que se agoten los nombres disponibles.
+
+## ¿Qué se uso?
+- HTML 5
+- CSS3
+- JavaScript
+
+## Estructura del proyecto
+
+├── index.html # Interfaz principal
+├── style.css # Estilos del proyecto
+├── app.js # Lógica del sorteo y funciones DOM
+└── README.md # Este archivo
+
+## Instrucciones de uso
+
+1. Clona este repositorio o descarga los archivos.
+2. Abre el archivo `index.html` en tu navegador.
+3. Ingresa los nombres de tus amigos en el input y presiona "Añadir".
+4. Haz clic en **"Sortear amigo"** para comenzar el sorteo.
+5. Sigue sorteando hasta que se hayan seleccionado todos los nombres.
+
+## Validcaiones
+
+- No permite tener el campo de nombre vacio.
+- No permite sorteo sin nombres ingresados
+- Sistema controla que no se repitan nombres sorteados
+- Alerta para avisar cuando ya se han sorteado todos los nombres
+
+## Autoría
+
+Desarrollado por **Enrique Abrham Céspedes Domínguez** como parte del Challenge de Alura Latam
